41009db59c32e5fac6b3742b824a16fcf7819421
jontymuriithi <muriithijohn634@gmail.com>
2023-12-05 09:39:00 -0800

    good

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
 
-import json
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-with open("add_item.json", "a+", encoding="UTF8") as file:
-    file.seek(0)
-    list_in_file = file.read()
+def main():
+    """
+    this method writes to file
+    """
 
-    if list_in_file:
-        list_in_file = json.loads(list_in_file)
+    sys = __import__('sys')
+    jsonn = __import__('json')
+    save_to_json_file = __import__('5-save_to_json_file')
+    load_from_json_file = __import__('6-load_from_json_file')
+    with open("add_item.json", "a+", encoding="UTF8") as file:
+        file.seek(0)
+        list_in_file = file.read()
 
-if type(list_in_file) != list:
-    list_in_file = []
+        if list_in_file:
+            list_in_file = jsonn.loads(list_in_file)
 
-for x in range(len(sys.argv)):
-    if x != 0:
-        list_in_file.append(sys.argv[x])
+    if type(list_in_file) != list:
+        list_in_file = []
 
-with open("add_item.json", "w", encoding="UTF8") as file:
-    file.write(json.dumps(list_in_file))
+    for x in range(len(sys.argv)):
+        if x != 0:
+            list_in_file.append(sys.argv[x])
+
+    with open("add_item.json", "w", encoding="UTF8") as file:
+        file.write(jsonn.dumps(list_in_file))
+
+if __name__ == "__main__":
+    main()

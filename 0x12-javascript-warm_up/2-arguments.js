b11100e756e8b3d82c035e6746cf10e3961b0268
johntymuriithi <muriithijohn634@gmail.com>
2024-02-12 12:34:23 +0300

    wrote some argumetns

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const { argv } = require('node:process');
 const len = argv.length;
 
-if (len === 1) {
+if (len === 2) {
   console.log('No argument');
-} else if (len === 2) {
+} else if (len === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');

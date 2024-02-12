0949acc770524af8d6f3a76595a13659a722bf5d
johntymuriithi <muriithijohn634@gmail.com>
2024-02-12 20:50:41 +0300

    first loop in for..of

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+const ar = [line1, line2, line3];
+
+for (const x of ar) {
+  console.log(x);
+}

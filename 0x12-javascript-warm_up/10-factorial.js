cf025507a28236d2612a7d72781f76ac0246b6a0
johntymuriithi <muriithijohn634@gmail.com>
2024-02-13 14:51:18 +0300

    coded factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@ const arg1 = parseInt(argv[2]); // Convert the argument to a number
 
 function factorial (n) {
   if (isNaN(n) === true) { // Check for NaN and negative numbers
-    return 'Invalid input';
+    return 1;
   }
   if (n === 0) { // Base case: factorial of 0 is 1
     return 1;

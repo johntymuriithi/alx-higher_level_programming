d1a250c61eb94e8afb7ad62d7634618e6051920a
johntymuriithi <muriithijohn634@gmail.com>
2024-02-14 05:46:36 +0300

    initialized my first class

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,14 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // Create an empty object if w or h is equal to 0 or not a positive integer
       return {};
     }
+
+    // Initialize the instance attributes width and height
     this.width = w;
     this.height = h;
   }
 }
+
 module.exports = Rectangle;

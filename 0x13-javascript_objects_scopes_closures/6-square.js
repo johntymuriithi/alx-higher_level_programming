55f737bae644106ccfaf0d1f52fec019fa8760e6
johntymuriithi <muriithijohn634@gmail.com>
2024-02-14 12:26:58 +0300

    created a subclass of Rectangel class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const square= require('./5-square');
 
-class Square extends Rectangle {
+class Square extends square {
   constructor (size) {
     super(size, size);
   }

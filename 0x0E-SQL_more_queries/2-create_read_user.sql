5657c584b53eb5c6589ddfb6d0fddd3ccf0bb6ba
johntymuriithi <muriithijohn634@gmail.com>
2024-03-23 13:22:01 +0300

    query2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,8 +3,8 @@
 -- password should be set to user_0d_1_pwd
 -- if user_0d_2 already exists, script should not fail
 
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT USAGE ON *.* TO `user_0d_2`@`localhost`;
-GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`  
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;

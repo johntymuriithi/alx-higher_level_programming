232fe7678eca4760500ce8c6d6e936aace7ddeae
johntymuriithi <muriithijohn634@gmail.com>
2024-03-23 13:18:30 +0300

    query2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,7 +3,7 @@
 -- password should be set to user_0d_1_pwd
 -- if user_0d_2 already exists, script should not fail
 
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-CREATE database 'hbtn_0d_2';
 GRANT SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;

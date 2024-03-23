e5cdf7a42ea56b1f98d777ebeab5d972c0d40de1
johntymuriithi <muriithijohn634@gmail.com>
2024-03-23 14:14:27 +0300

    query9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- lets continue combinng
+
+SELECT cities.id, cities.name AS city_name, states.name AS state_name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
